dab9f222a78c76db66cf96d7f0b76a5dbb7627bf
testgithubhar <145427363+testgithubhar@users.noreply.github.com>
2023-10-07 13:25:44 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,11 @@
+a = int(input("enter the value for a"))
+b = int(input("enter the value for b"))
+print("before swapping")
+print("value of a", a)
+print("value of b", b)
+temp = a
+a = b
+b = temp
+print("after swapping")
+print("value of a", a)
+print("value of b", b)
